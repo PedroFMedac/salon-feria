57b956d7e827964c4a2d30bfc5d8fbdd27fc0f16
Pedro <pedro.j.izdo@gmail.com>
2024-11-18 08:51:19 +0100

    ....

--- src/controllers/offersController.js
+++ src/controllers/offersController.js
@@ -80,7 +80,7 @@ const getOffersById = async (req, res) => {
 
         const userId = rol === 'co' ? id : req.body.companyID;
 
-        console.log('Datos enviados al backend:', { id: this.id });
+        console.log('Datos enviados al backend:', { id: id });
 
 
         if (!userId) {
